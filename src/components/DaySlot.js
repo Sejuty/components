456bf6981beac60078733f7db0fe17a702b14284
Sejuty <tafannumnishat00@gmail.com>
2023-04-12 12:54:25 +0600

    with start time and end time

--- src/components/DaySlot.js
+++ src/components/DaySlot.js
@@ -1,34 +1,67 @@
 import React from "react";
-import FourHours from "./FourHoursSlot";
+import AvailableTime from "../json/available-times.json";
 
 function TimeDivision({ day }) {
+  const availableTime = AvailableTime;
+
+  const start_time = availableTime[0].slots[0].start_time;
+  const end_time = availableTime[0].slots[0].end_time;
+
+  function timeConversion(number) {
+    if (number > 2359 || number < 1) return [];
+    const minutes = number % 100;
+    const hours = (number - minutes) / 100;
+    return [hours, minutes];
+  }
+
+  const [start_hours, start_minutes] = timeConversion(start_time);
+  const [end_hours, end_minutes] = timeConversion(end_time);
+
+  const trackStartHour = new Array(24).fill(false);
+  for (let i = 0; i < start_hours; i++) {
+    trackStartHour[i] = true;
+  }
+
+  const trackEndHour = new Array(24).fill(false);
+  for (let i = 0; i < end_hours; i++) {
+    trackEndHour[i] = true;
+  }
+
+  const divs = Array.from({ length: 24 }, (_, hour) => (
+    <div
+      key={hour}
+      className="w-1/6 h-10 bg-blue-100 border border-white relative"
+    >
+      {hour === end_hours ? (
+        <div
+          className="absolute inset-0 bg-blue-500"
+          style={{ width: `${(end_minutes * 100) / 60}%` }}
+        ></div>
+      ) : (
+        <div
+          className="absolute inset-0 bg-blue-500"
+          style={{ width: `${trackEndHour[hour] ? 100 : 0}%` }}
+        ></div>
+      )}
+      {hour === start_hours ? (
+        <div
+          className="absolute inset-0 bg-blue-100"
+          style={{ width: `${(start_minutes * 100) / 60}%` }}
+        ></div>
+      ) : (
+        <div
+          className="absolute inset-0 bg-blue-100"
+          style={{ width: `${trackStartHour[hour] ? 100 : 0}%` }}
+        ></div>
+      )}
+    </div>
+  ));
   return (
-    <div className="flex max-width-screen">
-      <div className="flex w-[50px] h-[30px] justify-end pr-2">{day}</div>
-      <div class="flex-auto bg-blue-100 border border-l-gray-400 border-white"></div>
-      <div class="flex-auto bg-blue-100 border border-white"></div>
-      <div class="flex-auto bg-blue-100 border border-white"></div>
-      <div class="flex-auto bg-blue-100 border border-white "></div>
-      <div class="flex-auto bg-blue-100 border border-white border-l-gray-400"></div>
-      <div class="flex-auto bg-blue-100 border border-white"></div>
-      <div class="flex-auto bg-blue-100 border border-white"></div>
-      <div class="flex-auto bg-blue-100 border border-white "></div>
-      <div class="flex-auto bg-blue-100 border border-white border-l-gray-400"></div>
-      <div class="flex-auto bg-blue-100 border border-white"></div>
-      <div class="flex-auto bg-blue-100 border border-white"></div>
-      <div class="flex-auto bg-blue-100 border border-white "></div>
-      <div class="flex-auto bg-blue-100 border border-white border-l-gray-400"></div>
-      <div class="flex-auto bg-blue-100 border border-white"></div>
-      <div class="flex-auto bg-blue-100 border border-white"></div>
-      <div class="flex-auto bg-blue-100 border border-white "></div>
-      <div class="flex-auto bg-blue-100 border border-white border-l-gray-400"></div>
-      <div class="flex-auto bg-blue-100 border border-white"></div>
-      <div class="flex-auto bg-blue-100 border border-white"></div>
-      <div class="flex-auto bg-blue-100 border border-white "></div>
-      <div class="flex-auto bg-blue-100 border border-white border-l-gray-400"></div>
-      <div class="flex-auto bg-blue-100 border border-white"></div>
-      <div class="flex-auto bg-blue-100 border border-white"></div>
-      <div class="flex-auto bg-blue-100 border border-white border-r-gray-400"></div>
+    <div className="flex">
+      <div className="flex w-[50px] h-[30px] justify-end pr-2 text-xl">
+        {day}
+      </div>
+      {divs}
     </div>
   );
 }
--- src/components/TimeSlot.js
+++ src/components/TimeSlot.js
@@ -6,21 +6,21 @@ function TimeSlot() {
     <div className="p-4">
       <div className="flex-col space-y-2 max-w-screen">
         <div className="flex">
-          <div className="w-1/6 pl-14">00:00</div>
-          <div className="w-1/6 pl-6">04:00</div>
-          <div className="w-1/6 pl-6">08:00</div>
-          <div className="w-1/6 pl-6">12:00</div>
-          <div className="w-1/6 pl-6">16:00</div>
-          <div className="w-1/6 pl-6">20:00</div>
+          <div className="w-1/6 text-xl pl-10">00:00</div>
+          <div className="w-1/6 text-xl pl-[15px]">04:00</div>
+          <div className="w-1/6 text-xl pl-[15px]">08:00</div>
+          <div className="w-1/6 text-xl pl-[15px]">12:00</div>
+          <div className="w-1/6 text-xl pl-[15px]">16:00</div>
+          <div className="w-1/6 text-xl pl-[15px]">20:00</div>
           <div>24:00</div>
         </div>
         <DaySlot day={"Sun"} />
-        <DaySlot day={"Mon"} />
+        {/* <DaySlot day={"Mon"} />
         <DaySlot day={"Tue"} />
         <DaySlot day={"Wed"} />
         <DaySlot day={"Thu"} />
         <DaySlot day={"Fri"} />
-        <DaySlot day={"Sat"} />
+        <DaySlot day={"Sat"} /> */}
       </div>
     </div>
   );
